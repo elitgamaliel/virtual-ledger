e6b4aef68fc05e7d8757c9aa7b71b59d905d6350
FlyingCat-X <flyingcatdm@gmail.com>
2021-10-29 11:10:59 -0500

    Remove read all method from Ledger

--- src/controller/ledger-controller.ts
+++ src/controller/ledger-controller.ts
@@ -1,39 +1,7 @@
 import express, { Request, Response } from "express";
-import { Ledger } from "../model/ledger";
-import { TransactionType } from "../model/transaction-type";
-import { User } from "../model/user";
 
 class LedgerController {
   async create(req: Request, res: Response) {}
-  async read(req: Request, res: Response) {
-    try {
-      const limit = req.query?.limit as number | undefined;
-      const offset = req.query?.offset as number | undefined;
-      const records = await Ledger.findAll({
-        where: {},
-        include: [
-          {
-            model: User,
-            as: "user",
-          },
-          {
-            model: TransactionType,
-            as: "transaction_type",
-          },
-        ],
-        limit,
-        offset,
-      });
-      return res.json(records);
-    } catch (e) {
-      return res.json({
-        msg: "Failed to read Ledgers",
-        status: 500,
-        jsMsg: (e as Error).message,
-        route: "/ledger",
-      });
-    }
-  }
   async readByID(req: Request, res: Response) {}
   async update(req: Request, res: Response) {}
   async delete(req: Request, res: Response) {}
--- src/route/ledger-route.ts
+++ src/route/ledger-route.ts
@@ -1,9 +1,5 @@
 import express from "express";
-import Middleware from "../middleware";
-import ledgerController from "../controller/ledger-controller";
 
 const router = express.Router();
-
-router.get("/", ledgerController.read);
 
 export default router;
