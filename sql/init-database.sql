17aa335e09896944f438ae18724366d8b5ca9ebc
FlyingCat-X <flyingcatdm@gmail.com>
2021-10-29 11:05:45 -0500

    Rename column

--- sql/init-database.sql
+++ sql/init-database.sql
@@ -14,25 +14,25 @@ USE system_db;
 -- Table: Users
 DROP TABLE IF EXISTS user;
 CREATE TABLE user (
-    id             INT NOT NULL AUTO_INCREMENT,
-    username       VARCHAR(64),
-    first_name     VARCHAR(64),
-    last_name      VARCHAR(64),
-    email          VARCHAR(64),
-    password       VARCHAR(64),
+    id                      INT NOT NULL AUTO_INCREMENT,
+    username                VARCHAR(64),
+    first_name              VARCHAR(64),
+    last_name               VARCHAR(64),
+    email                   VARCHAR(64),
+    password                VARCHAR(64),
     PRIMARY KEY (id)
 );
 
 -- Table: Ledger
 DROP TABLE IF EXISTS ledger;
 CREATE TABLE ledger (
-    id              INT NOT NULL AUTO_INCREMENT,
-    user_id         INT NOT NULL,
-    trans_type_id   INT NOT NULL,
-    amount          DECIMAL(13,2) NULL DEFAULT '0.00',
-    description     VARCHAR(128),
-    created_on      DATETIME,
-    added_on        DATETIME,
+    id                      INT NOT NULL AUTO_INCREMENT,
+    user_id                 INT NOT NULL,
+    transaction_type_id     INT NOT NULL,
+    amount                  DECIMAL(13,2) NULL DEFAULT '0.00',
+    description             VARCHAR(128),
+    created_on              DATETIME,
+    added_on                DATETIME,
     PRIMARY KEY (id)
 );
 
@@ -46,4 +46,4 @@ CREATE TABLE transaction_type (
 
 -- Foreign Keys
 ALTER TABLE ledger ADD CONSTRAINT fk_ledger__user FOREIGN KEY (user_id) REFERENCES user(id);
-ALTER TABLE ledger ADD CONSTRAINT fk_ledger__trans_type FOREIGN KEY (trans_type_id) REFERENCES transaction_type(id);
+ALTER TABLE ledger ADD CONSTRAINT fk_ledger__trans_type FOREIGN KEY (transaction_type_id) REFERENCES transaction_type(id);
