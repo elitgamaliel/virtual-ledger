190a3a3d1b73dce3d7dd265e78c14a8cd761a905
FlyingCat-X <flyingcatdm@gmail.com>
2021-10-29 12:14:25 -0500

    Add Read by ID to LedgerController

--- src/controller/ledger-controller.ts
+++ src/controller/ledger-controller.ts
@@ -1,8 +1,37 @@
 import express, { Request, Response } from "express";
+import { Ledger } from "../model/ledger";
+import { User } from "../model/user";
 
 class LedgerController {
   async create(req: Request, res: Response) {}
-  async readByID(req: Request, res: Response) {}
+  async readByID(req: Request, res: Response) {
+    try {
+      const { id } = req.params;
+      const limit = req.query?.limit as number | undefined;
+      const offset = req.query?.offset as number | undefined;
+      const records = await Ledger.findAll({
+        where: {
+          user_id: id,
+        },
+        include: [
+          {
+            model: User,
+            as: "user",
+          },
+        ],
+        limit,
+        offset,
+      });
+      return res.json(records);
+    } catch (e) {
+      return res.json({
+        msg: "Failed to read Ledgers",
+        status: 500,
+        jsMsg: (e as Error).message,
+        route: "/ledger/:id",
+      });
+    }
+  }
   async update(req: Request, res: Response) {}
   async delete(req: Request, res: Response) {}
 }
--- src/route/ledger-route.ts
+++ src/route/ledger-route.ts
@@ -1,5 +1,9 @@
 import express from "express";
+import Middleware from "../middleware";
+import ledgerController from "../controller/ledger-controller";
 
 const router = express.Router();
+
+router.get("/:id", ledgerController.readByID);
 
 export default router;
